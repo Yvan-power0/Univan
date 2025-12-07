cb3c9c1ae67e918e48b5f879c9dad821ef72868b
Yvan-power0 <koffimoayechristyvan@gmail.com>
2025-12-07 10:02:43 +0000

    Fix syntax issues in main.py

--- main.py
+++ main.py
@@ -1,55 +1,48 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
 
-app = Flask(__name__)
+app = Flask(name)
 socketio = SocketIO(app, logger=True, engineio_logger=True)
 connected_users = {}
 
-
 @app.route("/")
 def login():
-    return render_template("login.html")
-
+return render_template("login.html")
 
 @app.route("/chat", methods=["POST"])
 def chat():
-    username = request.form["username"]
-    room = request.form["room"]
-    return render_template("chat.html", username=username, room=room)
-
+username = request.form["username"]
+room = request.form["room"]
+return render_template("chat.html", username=username, room=room)
 
 @socketio.on("chat message")
 def handle_chat(json):
-    emit("chat message", json, broadcast=True, include_self=False, to=json["room"])
-
+emit("chat message", json, broadcast=True, include_self=False, to=json["room"])
 
 @socketio.on("writing")
 def handle_writing(json):
-    emit(
-        "writing",
-        json,
-        broadcast=True,
-        include_self=False,
-        to=json["room"],
-    )
-
+emit(
+"writing",
+json,
+broadcast=True,
+include_self=False,
+to=json["room"],
+)
 
 @socketio.on("connect")
 def on_connect(auth):
-    room = auth["room"]
-    username = auth["username"]
-    connected_users[request.sid] = (username, room)
-    join_room(room)
-    emit("connected users", connected_users, broadcast=True, to=room)
-
+room = auth["room"]
+username = auth["username"]
+connected_users[request.sid] = (username, room)
+join_room(room)
+emit("connected users", connected_users, broadcast=True, to=room)
 
 @socketio.on("disconnect")
 def on_disconnect():
-    room = connected_users[request.sid][1]
-    leave_room(room)
-    del connected_users[request.sid]
-    emit("connected users", connected_users, broadcast=True, to=room)
+room = connected_users[request.sid][1]
+leave_room(room)
+del connected_users[request.sid]
+emit("connected users", connected_users, broadcast=True, to=room)
 
-
-if __name__ == "__main__":
-    socketio.run(app, debug=True, use_reloader=True)
+if name == "main":
+socketio.run(app, debug=True, use_reloader=True)
